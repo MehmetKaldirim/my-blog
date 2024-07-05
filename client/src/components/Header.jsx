cf8765d9772f898e021aca6d13e48f6aa7296ff4
Mehmet <mathdenizi@gmail.com>
2024-07-05 13:30:10 +0200

    style problem

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,11 +32,13 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>
+            Sign In
+          </Button>
         </Link>
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse className="gap-2">
+      <Navbar.Collapse>
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
