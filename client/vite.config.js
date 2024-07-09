a1d9717552e44042a805d77150bb4ba74c509273
Mehmet <mathdenizi@gmail.com>
2024-07-09 02:26:04 +0200

    signup work properly

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001", // Adjust this to your backend server URL
+        changeOrigin: true,
+        secure: false,
+        // Comment out or remove the rewrite function
+        // rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    port: 5023,
+    open: true,
+  },
+});
