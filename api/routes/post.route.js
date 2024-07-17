41f2157c7348bbf310d9b66f2ed46d3ccb46eeb0
Mehmet <mathdenizi@gmail.com>
2024-07-17 23:19:16 +0200

    show user posts inside dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,6 +5,6 @@ import { create, getposts } from "../controllers/post.controller.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-router.get("/getpsts", getposts);
+router.get("/getposts", getposts);
 
 export default router;
