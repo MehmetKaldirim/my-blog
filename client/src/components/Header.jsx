abc045d5c35e258ff8232691a31e4282856599ce
Mehmet <mathdenizi@gmail.com>
2024-07-18 03:57:04 +0200

    update post functinality added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -96,7 +96,7 @@ export default function Header() {
           <Link to="/about">About</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
+          <Link to="/dashboard?tab=posts">Posts</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
