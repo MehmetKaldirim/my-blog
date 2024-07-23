07e3849fd569d54ac049c4f88e7b287e0798dd05
Mehmet <mathdenizi@gmail.com>
2024-07-23 05:13:15 +0200

    port changed for heroku

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -8,12 +9,12 @@ import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-dotenv.config();
+const port = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.MANGO_URI)
   .then(() => {
-    console.log("MongoDb is connected");
+    console.log("mongodb is connected");
   })
   .catch((err) => {
     console.log(err);
@@ -26,8 +27,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
 
 app.use("/api/user", userRoutes);
