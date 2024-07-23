3e672503e9890f3f1a72dcd281e1eb3c1849cf09
Mehmet <mathdenizi@gmail.com>
2024-07-23 03:18:46 +0200

    deploy 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5001", // Adjust this to your backend server URL
-        changeOrigin: true,
+        target: "http://localhost:3000",
         secure: false,
-        // Comment out or remove the rewrite function
-        // rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-    port: 5023,
-    open: true,
   },
+  plugins: [react()],
 });
